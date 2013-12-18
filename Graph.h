d32d16adc49b3fd6f520f6e1f5037217f8df0d52
adrlabor <adrien.laborderie@etu.u-bordeaux1.fr>
2013-12-18 14:38:39 +0100

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,12 +1,30 @@
 #ifndef GRAPH_H
-#define	GRAPH_H
+#define GRAPH_H
 
 #include <vector>
-#include <iostream>
 #include <string>
 
-class Graph{
+using namespace std;
 
+class Graph
+{
+    private:
+        vector<vector<int> > g_vertex;
+    public:
+        Graph();
+        Graph(vector<vector<int> > g_vertex);
+        virtual ~Graph();
+
+        int getNbVertex() const;
+        int addVertex();
+        void delVertex(int vertex);
+
+        int getNbEdge(int vertex) const;
+        virtual void addEdge(int vertex, int vertex2);
+        virtual void delEdge(int vertex1, int vertex2);
+
+        bool haveEdge(int vertex1, int vertex2);
+        string toString();
 };
-#endif	/* GRAPH_H */
 
+#endif // GRAPH_H
