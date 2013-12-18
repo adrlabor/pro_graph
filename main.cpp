1417b8c4a6920c2e161c7964e12e1d6f60f61c18
adrlabor <adrien.laborderie@etu.u-bordeaux1.fr>
2013-12-18 21:47:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 
 #include "include/Graph.h"
+#include "include/Random_graph.h"
+#include "include/Algorithmes.h"
+#include <vector>
 
 using namespace std;
 
+void displayVector(vector<int> v) {
+    vector<int>::iterator it;
+    cout << "Covers : ";
+    for (it = v.begin(); it != v.end(); ++it) {
+            cout << *it + 1 << "  ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-    //Création de sommets
-    vector<int> sommet1 = vector<int>();
-    vector<int> sommet2 = vector<int>();
-    vector<int> sommet3 = vector<int>();
-    sommet1.push_back(1);sommet1.push_back(2);
-    sommet2.push_back(0);sommet2.push_back(2);
-    sommet3.push_back(0);sommet3.push_back(1);
-    //Création d'un graphe
-    vector<vector<int> > g1 = vector<vector<int> >();
-    g1.push_back(sommet1);
-    g1.push_back(sommet2);
-    g1.push_back(sommet3);
+    Graph* g = new Random_graph(5, 0.5);
+    Graph* g1 = new Graph();
+    g1->addVertex();g1->addVertex();g1->addVertex();g1->addVertex();g1->addVertex();
+    g1->addEdge(0,1);g1->addEdge(0,2);g1->addEdge(1,2);g1->addEdge(1,3);g1->addEdge(3,4);
 
-    Graph g = Graph(g1);
-    cout << "Graph 1 :\n" << g.toString() << endl;
+    cout << "Graph 1 :\n" << g->toString() << endl;
 
-    //Suppression d'une arrête
+    /*//Suppression d'une arrête
     g.delEdge(0,2);
     cout << "Suppression arrête 1-3 :\n" << g.toString() << endl;
 
     //Suppression d'un sommet
-    /*g.delVertex(2);
-    cout << "Suppression sommet 3 :\n" << g.toString() << endl;*/
+    //g.delVertex(2);
+    //cout << "Suppression sommet 3 :\n" << g.toString() << endl;
 
     //Ajout de deux sommets
     g.addVertex();
@@ -39,7 +42,16 @@ int main()
     g.addEdge(2,3);
     g.addEdge(0,3);
     g.addEdge(1,3);
-    cout << "Ajout de 3 arrêtes :\n" << g.toString() << endl;
+    cout << "Ajout de 3 arrêtes :\n" << g.toString() << endl;*/
+
+    vector<int> cover = glouton(g);
+    displayVector(cover);
+
+    vector<int> cover2 = twoAppr_neighbourhood(g);
+    displayVector(cover2);
+
+    vector<int> cover3 = twoAppr_deepSearch(g1);
+    displayVector(cover3);
 
     return 0;
 }
