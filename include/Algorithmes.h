1d8e50621560cd53bc68f2fc9492a0b7dc97387b
adrlabor <adrien.laborderie@etu.u-bordeaux1.fr>
2013-12-19 00:13:07 +0100

    Update Algorithmes.h

--- include/Algorithmes.h
+++ include/Algorithmes.h
@@ -5,6 +5,7 @@
 
 vector<int> glouton(const Graph *graph);
 vector<int> twoAppr_neighbourhood(const Graph *graph);
-vector<int> twoAppr_deepSearch(const Graph *graph);
+vector<int> twoAppr_depthSearch(const Graph *graph);
+vector<int> opti_bipartis(const Graph *graph);
 
 #endif // ALGORITHMES_H
