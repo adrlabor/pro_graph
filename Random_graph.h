ed8301bd9a9a27a57c1613ea45196fe579921676
adrlabor <adrien.laborderie@etu.u-bordeaux1.fr>
2013-12-18 18:52:42 +0100

    Update Random_graph.h
    j'avais inversé .h et .cpp

--- Random_graph.h
+++ Random_graph.h
@@ -1,36 +1,16 @@
-#include "../include/Random_graph.h"
+#ifndef RANDOM_GRAPH_H
+#define RANDOM_GRAPH_H
 
-#include <time.h>
-#include <cstdlib>
+#include "../include/Graph.h"
 
-Random_graph::Random_graph()
+class Random_graph : public Graph
 {
-    //Création par défaut d'un graphe avec 5 sommets et 1 chance sur 2 de créer une arrête
-    generateRandomGraph(5, 0.5);
-}
+    public:
+        Random_graph();
+        Random_graph(int nbVertex, float probaEdge);
+        virtual ~Random_graph();
+    private:
+        void generateRandomGraph(int nbVertex, float probaEdge);
+};
 
-Random_graph::Random_graph(int nbVertex, float probaEdge)
-{
-    generateRandomGraph(nbVertex, probaEdge);
-}
-
-Random_graph::~Random_graph()
-{
-    //dtor
-}
-
-void Random_graph::generateRandomGraph(int nbVertex, float probaEdge){
-
-    srand(time(NULL));
-    if (nbVertex < 1)
-            return;
-    int i = addVertex();
-    while (i < nbVertex-1){
-        i = addVertex();
-        for (int j=i-1; j>=0; j--) {
-            double randomNumber = rand()/(double)RAND_MAX;
-            if(randomNumber < probaEdge)
-                addEdge(j, i);
-        }
-    }
-}
+#endif // RANDOM_GRAPH_H
